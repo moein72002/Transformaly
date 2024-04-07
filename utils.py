4a344ecf9349edc1b476ee36552e07c7f5b8f857
Moein Madadi <moeinmadadi2002@gmail.com>
2024-04-07 15:46:36 +0330

    moves evaluate_method from eval.py to util.py

--- utils.py
+++ utils.py
@@ -27,7 +27,21 @@ from torchvision.datasets import CIFAR10, CIFAR100, FashionMNIST, ImageFolder
 from torchvision.transforms import Compose
 from datasets.wbc1 import get_wbc1_train_and_test_dataset_for_anomaly_detection, get_wbc1_id_test_dataset, get_just_wbc1_test_dataset_for_anomaly_detection
 from datasets.wbc2 import get_wbc2_train_and_test_dataset_for_anomaly_detection, get_wbc2_id_test_dataset, get_just_wbc2_test_dataset_for_anomaly_detection
-from eval import evaluate_method
+
+import logging
+from os.path import join
+import pandas as pd
+import numpy as np
+from numpy.linalg import eig
+from sklearn.metrics import roc_auc_score
+from sklearn.decomposition import PCA
+from sklearn import mixture
+import torch.nn
+from utils import print_and_add_to_log, get_datasets_for_ViT, \
+    Identity, get_finetuned_features, knn_score
+from pytorch_pretrained_vit.model import AnomalyViT
+from datasets.wbc1 import get_wbc1_train_and_test_dataset_for_anomaly_detection, get_just_wbc1_test_dataset_for_anomaly_detection
+from datasets.wbc2 import get_wbc2_train_and_test_dataset_for_anomaly_detection, get_just_wbc2_test_dataset_for_anomaly_detection
 
 
 class DiorDataset(Dataset):
@@ -905,3 +919,306 @@ def plot_loss(train_losses, val_losses, to_show=True,
         plt.savefig(f'{path_to_save}/loss_graph.png')
     if to_show:
         plt.show()
+
+def evaluate_method(args=None, BASE_PATH=None, _classes=None):
+    logging.basicConfig(
+        filename=join(BASE_PATH,
+                      f'{"unimodal" if args["unimodal"] else "multimodal"}/{args["dataset"]}',
+                      f'Eval_{args["dataset"]}_Transformaly_outputs.log'), level=logging.DEBUG)
+    print_and_add_to_log("========================================================",
+                         logging)
+    print_and_add_to_log("Args are:", logging)
+    print_and_add_to_log(args, logging)
+    print_and_add_to_log("========================================================",
+                         logging)
+    results = {'class': [],
+               'pretrained_AUROC_scores': [],
+               'just_test_pretrained_AUROC_scores': [],
+               'eval_using_knn_pretrained_AUROC_scores': [],
+               'just_test_using_knn_pretrained_AUROC_scores': [],
+               'all_layers_finetuned_AUROC_scores': [],
+               'just_test_all_layers_finetuned_AUROC_scores': [],
+               'pretrained_and_finetuned_AUROC_scores': [],
+               'just_test_pretrained_and_finetuned_AUROC_scores': []}
+    for _class in _classes:
+        print_and_add_to_log("===================================", logging)
+        print_and_add_to_log(f"Class is : {_class}", logging)
+        print_and_add_to_log("===================================", logging)
+        args['_class'] = _class
+        base_feature_path = join(
+            BASE_PATH,
+            f'{"unimodal" if args["unimodal"] else "multimodal"}/{args["dataset"]}/class_{_class}')
+        model_path = join(base_feature_path, 'model')
+
+        args['base_feature_path'] = base_feature_path
+        args['model_path'] = model_path
+
+        # create the relevant directories
+        if not os.path.exists(model_path):
+            os.makedirs(model_path)
+
+        if args['unimodal']:
+            anomaly_classes = [i for i in _classes if i != args['_class']]
+        else:
+            anomaly_classes = [args['_class']]
+
+        results['class'].append(args['_class'])
+
+        if args['dataset'] == 'wbc1':
+            trainset, testset = get_wbc1_train_and_test_dataset_for_anomaly_detection()
+            anomaly_targets = [0 if label == testset.normal_class_label else 1 for label in testset.targets]
+            just_testset = get_just_wbc2_test_dataset_for_anomaly_detection()
+            just_test_anomaly_targets = [0 if label == just_testset.normal_class_label else 1 for label in
+                                         just_testset.targets]
+        elif args['dataset'] == 'wbc2':
+            trainset, testset = get_wbc2_train_and_test_dataset_for_anomaly_detection()
+            anomaly_targets = [0 if label == testset.normal_class_label else 1 for label in testset.targets]
+            just_testset = get_just_wbc1_test_dataset_for_anomaly_detection()
+            just_test_anomaly_targets = [0 if label == just_testset.normal_class_label else 1 for label in
+                                         just_testset.targets]
+        else:
+            trainset, testset = get_datasets_for_ViT(dataset=args['dataset'],
+                                                     data_path=args['data_path'],
+                                                     one_vs_rest=args['unimodal'],
+                                                     _class=args['_class'],
+                                                     normal_test_sample_only=False,
+                                                     use_imagenet=args['use_imagenet'],
+                                                     )
+            anomaly_targets = [0 if i in anomaly_classes else 1 for i in testset.targets]
+
+        test_loader = torch.utils.data.DataLoader(testset,
+                                                  batch_size=args['batch_size'],
+                                                  shuffle=False)
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            just_test_loader = torch.utils.data.DataLoader(just_testset,
+                                                           batch_size=args['batch_size'],
+                                                           shuffle=False)
+        train_loader = torch.utils.data.DataLoader(trainset,
+                                                   batch_size=args['batch_size'],
+                                                   shuffle=False)
+
+        print_and_add_to_log("=====================================================",
+                             logging)
+
+        # get ViT features
+        with open(join(BASE_PATH, f'{"unimodal" if args["unimodal"] else "multimodal"}',
+                       f'{args["dataset"]}/class_{args["_class"]}/extracted_features',
+                       'train_pretrained_ViT_features.npy'), 'rb') as f:
+            train_features = np.load(f)
+
+        with open(join(BASE_PATH, f'{"unimodal" if args["unimodal"] else "multimodal"}',
+                       f'{args["dataset"]}/class_{args["_class"]}/extracted_features',
+                       'test_pretrained_ViT_features.npy'), 'rb') as f:
+            test_features = np.load(f)
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            with open(join(BASE_PATH, f'{"unimodal" if args["unimodal"] else "multimodal"}',
+                           f'{args["dataset"]}/class_{args["_class"]}/extracted_features',
+                           'just_test_pretrained_ViT_features.npy'), 'rb') as f:
+                just_test_features = np.load(f)
+
+        # estimate the number of components
+        cov_train_features = np.cov(train_features.T)
+        values, vectors = eig(cov_train_features)
+        sorted_vals = sorted(values, reverse=True)
+        cumsum_vals = np.cumsum(sorted_vals)
+        explained_vars = cumsum_vals / cumsum_vals[-1]
+
+        for i, explained_var in enumerate(explained_vars):
+            n_components = i
+            if explained_var > args['whitening_threshold']:
+                break
+
+        print_and_add_to_log("=======================", logging)
+        print_and_add_to_log(f"number of components are: {n_components}", logging)
+        print_and_add_to_log("=======================", logging)
+
+        pca = PCA(n_components=n_components, svd_solver='full', whiten=True)
+        train_features = np.ascontiguousarray(pca.fit_transform(train_features))
+        test_features = np.ascontiguousarray(pca.transform(test_features))
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            just_test_features = np.ascontiguousarray(pca.transform(just_test_features))
+        print_and_add_to_log("Whitening ended", logging)
+
+        # build GMM
+        dens_model = mixture.GaussianMixture(n_components=1,
+                                             max_iter=1000,
+                                             verbose=1,
+                                             n_init=1)
+        dens_model.fit(train_features)
+        test_pretrained_samples_likelihood = dens_model.score_samples(test_features)
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            just_test_pretrained_samples_likelihood = dens_model.score_samples(just_test_features)
+        print_and_add_to_log("----------------------", logging)
+
+        pretrained_auc = roc_auc_score(anomaly_targets, test_pretrained_samples_likelihood)
+
+        eval_using_knn_distances = knn_score(train_features, test_features, n_neighbours=2)
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            just_test_pretrained_auc = roc_auc_score(just_test_anomaly_targets, just_test_pretrained_samples_likelihood)
+            just_test_eval_using_knn_distances = knn_score(train_features, just_test_features, n_neighbours=2)
+
+        print_and_add_to_log(f"Pretrained AUROC score is: {pretrained_auc}", logging)
+        print_and_add_to_log("----------------------", logging)
+        results['pretrained_AUROC_scores'].append(pretrained_auc)
+        results['eval_using_knn_pretrained_AUROC_scores'].append(eval_using_knn_distances)
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            results['just_test_pretrained_AUROC_scores'].append(just_test_pretrained_auc)
+            results['just_test_using_knn_pretrained_AUROC_scores'].append(just_test_eval_using_knn_distances)
+
+        # get finetuned prediction head scores
+        FINETUNED_PREDICTION_FILE_NAME = 'full_test_finetuned_scores.npy'
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            JUST_TEST_FINETUNED_PREDICTION_FILE_NAME = 'just_full_test_finetuned_scores.npy'
+
+        if args['use_imagenet']:
+            VIT_MODEL_NAME = 'B_16_imagenet1k'
+        else:
+            VIT_MODEL_NAME = 'B_16'
+
+        # load best instance
+        # Build model
+        if args["test_every_epoch"] or not os.path.exists(join(base_feature_path, 'features_distances',
+                                   FINETUNED_PREDICTION_FILE_NAME)):
+
+            print_and_add_to_log("Load Model", logging)
+            model_checkpoint_path = join(model_path, 'best_full_finetuned_model_state_dict.pkl')
+            model = AnomalyViT(VIT_MODEL_NAME, pretrained=True)
+            model.fc = Identity()
+            model_state_dict = torch.load(model_checkpoint_path)
+            ret = model.load_state_dict(model_state_dict)
+            print_and_add_to_log(
+                'Missing keys when loading pretrained weights: {}'.format(ret.missing_keys),
+                logging)
+            print_and_add_to_log(
+                'Unexpected keys when loading pretrained weights: {}'.format(ret.unexpected_keys),
+                logging)
+            print_and_add_to_log("model loadded from checkpoint here:", logging)
+            print_and_add_to_log(model_checkpoint_path, logging)
+            model = model.to('cuda')
+            model.eval()
+
+            test_finetuned_features = get_finetuned_features(model=model,
+                                                             loader=test_loader)
+            if args['dataset'] in ['wbc1', 'wbc2']:
+                just_test_finetuned_features = get_finetuned_features(model=model,
+                                                                      loader=just_test_loader)
+
+            if not os.path.exists(join(base_feature_path, 'features_distances')):
+                os.makedirs(join(base_feature_path, 'features_distances'))
+            np.save(join(base_feature_path, 'features_distances', FINETUNED_PREDICTION_FILE_NAME),
+                    test_finetuned_features)
+            if args['dataset'] in ['wbc1', 'wbc2']:
+                np.save(join(base_feature_path, 'features_distances', JUST_TEST_FINETUNED_PREDICTION_FILE_NAME),
+                        just_test_finetuned_features)
+
+        else:
+            test_finetuned_features = np.load(
+                join(base_feature_path, 'features_distances', FINETUNED_PREDICTION_FILE_NAME))
+            if args['dataset'] in ['wbc1', 'wbc2']:
+                just_test_finetuned_features = np.load(
+                    join(base_feature_path, 'features_distances', JUST_TEST_FINETUNED_PREDICTION_FILE_NAME))
+
+        if test_finetuned_features.shape[0] == 1:
+            test_finetuned_features = test_finetuned_features[0]
+
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            if just_test_finetuned_features.shape[0] == 1:
+                just_test_finetuned_features = just_test_finetuned_features[0]
+
+        # TODO: check below
+        if args["use_layer_outputs"] is None:
+            assert test_finetuned_features.shape[1] == just_test_finetuned_features.shape[1]
+            args["use_layer_outputs"] = list(range(test_finetuned_features.shape[1]))
+
+        # TODO: delete file if you want to call eval multiple times, I can add boolean as method parameter
+        if args["test_every_epoch"] or not os.path.exists(join(base_feature_path,
+                                   'features_distances', 'train_finetuned_features.npy')):
+            print_and_add_to_log("Load Model", logging)
+            model_checkpoint_path = join(model_path, 'best_full_finetuned_model_state_dict.pkl')
+            model = AnomalyViT(VIT_MODEL_NAME, pretrained=True)
+            model.fc = Identity()
+            model_state_dict = torch.load(model_checkpoint_path)
+            ret = model.load_state_dict(model_state_dict)
+            print_and_add_to_log(
+                'Missing keys when loading pretrained weights: {}'.format(ret.missing_keys),
+                logging)
+            print_and_add_to_log(
+                'Unexpected keys when loading pretrained weights: {}'.format(ret.unexpected_keys),
+                logging)
+            print_and_add_to_log("model loadded from checkpoint here:", logging)
+            print_and_add_to_log(model_checkpoint_path, logging)
+            model = model.to('cuda')
+
+            train_finetuned_features = get_finetuned_features(model=model,
+                                                              loader=train_loader)
+            np.save(join(base_feature_path, 'features_distances', 'train_finetuned_features.npy'),
+                    train_finetuned_features)
+        else:
+            train_finetuned_features = np.load(join(base_feature_path, 'features_distances',
+                                                    'train_finetuned_features.npy'))
+
+        if train_finetuned_features.shape[0] == 1:
+            train_finetuned_features = train_finetuned_features[0]
+            print_and_add_to_log("squeeze training features", logging)
+
+        train_finetuned_features = train_finetuned_features[:, args['use_layer_outputs']]
+        test_finetuned_features = test_finetuned_features[:, args['use_layer_outputs']]
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            just_test_finetuned_features = just_test_finetuned_features[:, args['use_layer_outputs']]
+        gmm_scores = []
+        train_gmm_scores = []
+        gmm = mixture.GaussianMixture(n_components=1,
+                                      max_iter=500,
+                                      verbose=1,
+                                      n_init=1)
+        gmm.fit(train_finetuned_features)
+        test_finetuned_samples_likelihood = gmm.score_samples(test_finetuned_features)
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            just_test_finetuned_samples_likelihood = gmm.score_samples(just_test_finetuned_features)
+        # train_finetuned_samples_likelihood = gmm.score_samples(train_finetuned_features)
+        # max_train_finetuned_features = np.max(np.abs(train_finetuned_samples_likelihood), axis=0)
+
+        test_finetuned_auc = roc_auc_score(anomaly_targets, test_finetuned_samples_likelihood)
+        print_and_add_to_log(f"All Block outputs prediciton AUROC score is: {test_finetuned_auc}",
+                             logging)
+
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            just_test_finetuned_auc = roc_auc_score(just_test_anomaly_targets, just_test_finetuned_samples_likelihood)
+            print_and_add_to_log(f"Just test all Block outputs prediciton AUROC score is: {just_test_finetuned_auc}",
+                                 logging)
+        results['all_layers_finetuned_AUROC_scores'].append(test_finetuned_auc)
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            results['just_test_all_layers_finetuned_AUROC_scores'].append(just_test_finetuned_auc)
+
+        print_and_add_to_log("----------------------", logging)
+
+        finetuned_and_pretrained_samples_likelihood = [
+            test_finetuned_samples_likelihood[i] + test_pretrained_samples_likelihood[i] for i in
+            range(len(test_pretrained_samples_likelihood))]
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            just_test_finetuned_and_pretrained_samples_likelihood = [
+                just_test_finetuned_samples_likelihood[i] + just_test_pretrained_samples_likelihood[i] for i in
+                range(len(just_test_pretrained_samples_likelihood))]
+
+        finetuned_and_pretrained_auc = roc_auc_score(anomaly_targets,
+                                                     finetuned_and_pretrained_samples_likelihood)
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            just_test_finetuned_and_pretrained_auc = roc_auc_score(just_test_anomaly_targets,
+                                                                   just_test_finetuned_and_pretrained_samples_likelihood)
+        print_and_add_to_log(
+            f"The bgm and output prediction prediciton AUROC is: {finetuned_and_pretrained_auc}",
+            logging)
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            print_and_add_to_log(
+                f"Just test: the bgm and output prediction prediciton AUROC is: {just_test_finetuned_and_pretrained_auc}",
+                logging)
+
+        results['pretrained_and_finetuned_AUROC_scores'].append(finetuned_and_pretrained_auc)
+        if args['dataset'] in ['wbc1', 'wbc2']:
+            results['just_test_pretrained_and_finetuned_AUROC_scores'].append(just_test_finetuned_and_pretrained_auc)
+    results_pd = pd.DataFrame.from_dict(results)
+    results_dict_path = join(BASE_PATH,
+                             f'summarize_results/{args["dataset"]}/{args["dataset"]}_results.csv')
+    if not os.path.exists(join(BASE_PATH, f'summarize_results/{args["dataset"]}')):
+        os.makedirs(join(BASE_PATH, f'summarize_results/{args["dataset"]}'))
+    results_pd.to_csv(results_dict_path)
